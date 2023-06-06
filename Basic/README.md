5d58b4ec07d2888d540da4d89f5d96ef59af99b1
Rjnishant <singhnishant530@gmail.com>
2023-06-06 23:13:22 +0530

    docs<Basic/README.md> Local Setup info

--- Basic/README.md
+++ Basic/README.md
@@ -1,1 +1,20 @@
-TO-DO Documentation 
+# Basic RSsSetup
+
+## Local Windows/Linux Setup
+
+1. Clone each file locally.
+2. Make sure you have docker and docker compose is installed.
+3. Change the environment variables **TZ** in the [docker-compose file](./docker-compose.yml) to your country value.
+4. Run `docker compose up`
+5. Go to http://localhost
+6. Do the initial one time FreshRss setup.
+7. Don't change Database Configuration for FreshRss in setup. Let it be the default "SQLite"
+8. Refer [FreshRss](https://github.com/FreshRSS/FreshRSS) [first steps documentation](https://freshrss.github.io/FreshRSS/en/users/02_First_steps.html) for more.
+9. ***CAUTION*** To Delete all Data and containers run `docker compose down -v`
+
+## Cloud Setup in AWS
+
+## Cloud Setup in Oracle
+
+
+
--- Basic/config.php
+++ Basic/config.php
@@ -1,24 +1,24 @@
 <?php
 return array (
   'environment' => 'production',
-  'salt' => '',
-  'base_url' => getenv('BASE_URL'),
+  'salt' => 'b02020c89d66bc6c58b4df99aae1a96083670f5b',
+  'base_url' => 'http://freshRss',
   'auto_update_url' => 'https://update.freshrss.org',
   'language' => 'en',
   'title' => 'FreshRSS',
   'meta_description' => '',
   'logo_html' => '',
-  'default_user' => '_',
+  'default_user' => 'hello123',
   'force_email_validation' => false,
   'allow_anonymous' => false,
   'allow_anonymous_refresh' => false,
   'auth_type' => 'form',
   'http_auth_auto_register' => true,
   'http_auth_auto_register_email_field' => '',
-  'api_enabled' => true,
+  'api_enabled' => false,
   'unsafe_autologin_enabled' => false,
   'simplepie_syslog_enabled' => true,
-  'pubsubhubbub_enabled' => false,
+  'pubsubhubbub_enabled' => true,
   'allow_robots' => false,
   'allow_referrer' => false,
   'limits' => 
@@ -37,11 +37,11 @@ return array (
   'db' => 
   array (
     'type' => 'sqlite',
-    'host' => 'localhost',
-    'user' => '',
-    'password' => '',
-    'base' => '',
-    'prefix' => 'freshrss_',
+    'host' => false,
+    'user' => false,
+    'password' => false,
+    'base' => false,
+    'prefix' => false,
     'connection_uri_params' => '',
     'pdo_options' => 
     array (
@@ -65,7 +65,7 @@ return array (
     'Google-Groups' => true,
     'Tumblr-GDPR' => true,
   ),
-  'disable_update' => false,
+  'disable_update' => true,
   'trusted_sources' => 
   array (
     0 => '127.0.0.0/8',
